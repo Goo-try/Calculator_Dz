de63269a46c211b7329c27ceafaac44f449c33a1
admin <admin@DESKTOP-164BMMH>
2025-12-06 00:57:29 +0300

    Кнопка 1 работает, потребовалась пара попыток, ладно не пара, примерно 525 =)

--- app/src/main/java/com/example/calculator_dz/MainActivity.java
+++ app/src/main/java/com/example/calculator_dz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.calculator_dz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView EditText;
+    MaterialButton button_1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        EditText = findViewById(R.id.editText);
+
+        assignId(button_1, R.id.button1);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+    }
+
+    void assignId(MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        EditText.setText(buttonText);
     }
 }
