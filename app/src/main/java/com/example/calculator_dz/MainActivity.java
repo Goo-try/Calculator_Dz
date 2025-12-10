604f87af46fd53924ec53177b21ce21ade3df640
admin <admin@DESKTOP-164BMMH>
2025-12-10 20:20:08 +0300

    up

--- app/src/main/java/com/example/calculator_dz/MainActivity.java
+++ app/src/main/java/com/example/calculator_dz/MainActivity.java
@@ -15,9 +15,6 @@ import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView edit_Text, solution_Text;
-    MaterialButton button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_0;
-    MaterialButton button_AC, buttonOpen_Bracket, buttonClose_Bracket, button_Divide, button_Multiply, button_Minus, button_Plus, button_C, button_Dot, button_Equal;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +26,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         solution_Text = findViewById(R.id.solutionText);
 
 
-        assignId(button_1, R.id.button1);
-        assignId(button_2, R.id.button2);
-        assignId(button_3, R.id.button3);
-        assignId(button_4, R.id.button4);
-        assignId(button_5, R.id.button5);
-        assignId(button_6, R.id.button6);
-        assignId(button_7, R.id.button7);
-        assignId(button_8, R.id.button8);
-        assignId(button_9, R.id.button9);
-        assignId(button_0, R.id.button0);
-        assignId(button_AC, R.id.buttonAC);
-        assignId(buttonOpen_Bracket, R.id.buttonOpenBracket);
-        assignId(buttonClose_Bracket, R.id.buttonCloseBracket);
-        assignId(button_Divide, R.id.buttonDivide);
-        assignId(button_Multiply, R.id.buttonMultiply);
-        assignId(button_Minus, R.id.buttonMinus);
-        assignId(button_Plus, R.id.buttonPlus);
-        assignId(button_C, R.id.buttonC);
-        assignId(button_Dot, R.id.buttonDot);
-        assignId(button_Equal, R.id.buttonEqual);
+        assignId(R.id.button1);
+        assignId(R.id.button2);
+        assignId(R.id.button3);
+        assignId(R.id.button4);
+        assignId(R.id.button5);
+        assignId(R.id.button6);
+        assignId(R.id.button7);
+        assignId(R.id.button8);
+        assignId(R.id.button9);
+        assignId(R.id.button0);
+        assignId(R.id.buttonAC);
+        assignId(R.id.buttonOpenBracket);
+        assignId(R.id.buttonCloseBracket);
+        assignId(R.id.buttonDivide);
+        assignId(R.id.buttonMultiply);
+        assignId(R.id.buttonMinus);
+        assignId(R.id.buttonPlus);
+        assignId(R.id.buttonC);
+        assignId(R.id.buttonDot);
+        assignId(R.id.buttonEqual);
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
@@ -60,11 +57,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    void assignId(MaterialButton btn, int id) {
-        btn = findViewById(id);
+    void assignId(int id) {
+        MaterialButton btn = findViewById(id);
         btn.setOnClickListener(this);
-
-
     }
 
 
@@ -75,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String dataCalculate = solution_Text.getText().toString();
 
         if(buttonText.equals("AC")){
-            solution_Text.setText("0");
+            solution_Text.setText("");
             return;
         }
 
@@ -87,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(buttonText.equals("=")){
             String result = getResult(dataCalculate);
             edit_Text.setText(result);
-            solution_Text.setText(result);
             return;
         }
 
@@ -98,13 +92,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         edit_Text.setText(buttonText);
 
-
     }
 
     String getResult(String data) {
-        Expression expression = new ExpressionBuilder(data).build();
-        double result = expression.evaluate();
-        return String.valueOf(result);
+        try {
+            Expression expression = new ExpressionBuilder(data).build();
+            double result = expression.evaluate();
+            return String.valueOf(result);
+        } catch (Exception e) {
+            return "Error";
+        }
     }
 
 }
